014b9365924ffcbc33b8c3780971ddba61d952eb
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-07-18 13:49:10 +0100

    implemented in-memory version of Token bucket algorithm

---
+++ src/rate_limiter/algorithms/token_bucket.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+class TokenBucket:
+    def __init__(self, capacity, refill_rate, refill_time):
+        self.capacity = capacity
+        self.refill_rate = refill_rate
+        self.refill_time = refill_time
+        self.buckets = {}
+
+    def _refill(self, key):
+        tokens, last_time = self.buckets[key]
+        now = datetime.now()
+        elapsed = (now - last_time).total_seconds()
+        added_tokens = (elapsed / self.refill_time) * self.refill_rate
+        tokens = min(self.capacity, tokens + added_tokens)
+        
+        self.buckets[key] = (tokens, now)
+
+    def _allow_request(self, key):
+        if key not in self.buckets:
+            self.buckets[key] = (self.capacity, datetime.now())
+
+        self._refill(key)
+        token, last_time = self.buckets[key]
+
+        if token >= 1:
+            self.buckets[key] = (token - 1, last_time)
+            return True
+        return False
+
+    def get_token_info(self, key):
+        if key not in self.buckets:
+            self.buckets[key] = (self.capacity, datetime.now())
+
+        self._refill(key)
+        return self.buckets[key]
